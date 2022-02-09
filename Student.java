1cd2139fff7f0afc0f8c797d548adaebb6870971
oksana-kolesnyk <99103668+oksana-kolesnyk@users.noreply.github.com>
2022-02-10 01:55:19 +0200

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+package task3.classes;
+
+public class Student {
+
+    public String RunTask (String name) {
+        String result = name + " has scored 3 points for Task3";
+        return result;
+    }
+
+
+
+}
